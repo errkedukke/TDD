be1ce7fb5495b95540869791fd1c52f4d419facd
Guga Rukhadze <ruxadze0@gmail.com>
2024-12-11 17:33:51 +0400

    Add project files.

---
+++ TDD.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TDD.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
