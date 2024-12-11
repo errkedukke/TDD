bc0e4fbf1c2d5bc1dcf77c50c2604817d386fb31
Guga Rukhadze <ruxadze0@gmail.com>
2024-12-11 18:15:33 +0400

    Added new test method.
    Added new test method to check if number devided by three would return Fizz or not.

--- TDD.UnitTests/FizzBuzzTests.cs
+++ TDD.UnitTests/FizzBuzzTests.cs
@@ -24,4 +24,22 @@ public class FizzBuzzTests
             Assert.That(result, Is.InRange(1, 100));
         });
     }
+
+    [Test]
+    public void WriteNumber_ResultIsFizzWhenDevisibleByThree()
+    {
+        // Arrange
+        var number = 30;
+
+        // Act
+        dynamic result = FizzBuzz.WriteNumber(number);
+
+        // Assert
+        Assert.Multiple(() => 
+        {
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.TypeOf<string>());
+            Assert.That(result, Is.EqualTo("Fizz"));
+        });
+    }
 }
